TrinityCore/TrinityCore

Core/Players: Add separate config option for starting money for Death Knight characters (#30466)

src/server/game/Entities/Player/Player.cpp
@@ -543,7 +543,9 @@ bool Player::Create(ObjectGuid::LowType guidlow, CharacterCreateInfo* createInfo
 
     InitRunes();
 
-    SetMoney(sWorld->getIntConfig(CONFIG_START_PLAYER_MONEY));
+    SetMoney(GetClass() != CLASS_DEATH_KNIGHT
+        ? sWorld->getIntConfig(CONFIG_START_PLAYER_MONEY)
+        : sWorld->getIntConfig(CONFIG_START_DEATH_KNIGHT_PLAYER_MONEY));
     SetHonorPoints(sWorld->getIntConfig(CONFIG_START_HONOR_POINTS));
     SetArenaPoints(sWorld->getIntConfig(CONFIG_START_ARENA_POINTS));
 

bgfrinci-creator/StefalWoW

(cherry picked from commit 65e2474ff264e40f579ce2aa29921e9d72c75c6f)

src/server/game/Entities/Player/Player.cpp
@@ -477,7 +477,9 @@ bool Player::Create(ObjectGuid::LowType guidlow, WorldPackets::Character::Charac
 
     InitRunes();
 
-    SetUpdateFieldValue(m_values.ModifyValue(&Player::m_activePlayerData).ModifyValue(&UF::ActivePlayerData::Coinage), sWorld->GetUInt64Config(CONFIG_START_PLAYER_MONEY));
+    SetUpdateFieldValue(m_values.ModifyValue(&Player::m_activePlayerData).ModifyValue(&UF::ActivePlayerData::Coinage), GetClass() != CLASS_DEATH_KNIGHT
+        ? sWorld->GetUInt64Config(CONFIG_START_PLAYER_MONEY)
+        : sWorld->GetUInt64Config(CONFIG_START_DEATH_KNIGHT_PLAYER_MONEY));
 
     // Played time
     m_Last_tick = GameTime::GetGameTime();
TrinityCore/TrinityCore

Scripts/Arcatraz: Update Zereketh (#31125)

src/server/scripts/Outland/TempestKeep/arcatraz/boss_zereketh_the_unbound.cpp
@@ -17,6 +17,8 @@
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "SpellInfo.h"
+#include "SpellMgr.h"
 #include "arcatraz.h"
 
 enum ZerekethTexts
@@ -49,11 +51,21 @@ struct boss_zereketh_the_unbound : public BossAI
     void JustEngagedWith(Unit* who) override
     {
         BossAI::JustEngagedWith(who);
-        events.ScheduleEvent(EVENT_VOID_ZONE, 6s, 10s);
-        events.ScheduleEvent(EVENT_SHADOW_NOVA, 6s, 10s);
-        events.ScheduleEvent(EVENT_SEED_OF_CORRUPTION, 12s, 20s);
+        events.ScheduleEvent(EVENT_VOID_ZONE, 10s, 15s);
+        events.ScheduleEvent(EVENT_SHADOW_NOVA, 15s, 20s);
+        events.ScheduleEvent(EVENT_SEED_OF_CORRUPTION, 5s, 10s);
         Talk(SAY_AGGRO);
     }
+
+    void OnSpellCast(SpellInfo const* spell) override
+    {
+        if (spell->Id == sSpellMgr->GetSpellIdForDifficulty(SPELL_SHADOW_NOVA, me))
+            if (roll_chance_i(50))
+                Talk(SAY_SHADOW_NOVA);
+    }
+
+    // Do not despawn Void Zone
+    void JustSummoned(Creature* /*summon*/) override { }
 
     void KilledUnit(Unit* /*victim*/) override
     {
@@ -81,19 +93,18 @@ struct boss_zereketh_the_unbound : public BossAI
             switch (eventId)
             {
                 case EVENT_VOID_ZONE:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 1, 100, true))
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
                         DoCast(target, SPELL_VOID_ZONE);
-                    events.Repeat(6s, 10s);
+                    events.Repeat(15s);
                     break;
                 case EVENT_SHADOW_NOVA:
-                    DoCastVictim(SPELL_SHADOW_NOVA);
-                    Talk(SAY_SHADOW_NOVA);
-                    events.Repeat(6s, 10s);
+                    DoCastSelf(SPELL_SHADOW_NOVA);
+                    events.Repeat(15s, 20s);
                     break;
                 case EVENT_SEED_OF_CORRUPTION:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 1, 100, true))
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
                         DoCast(target, SPELL_SEED_OF_CORRUPTION);
-                    events.Repeat(12s, 20s);
+                    events.Repeat(15s, 20s);
                     break;
                 default:
                     break;

bgfrinci-creator/StefalWoW

(cherry picked from commit 5715b96d02252c31191b1dbbcf3df13193301ffc)

src/server/scripts/Outland/TempestKeep/arcatraz/boss_zereketh_the_unbound.cpp
@@ -16,8 +16,9 @@
  */
 
 #include "ScriptMgr.h"
-#include "arcatraz.h"
 #include "ScriptedCreature.h"
+#include "SpellInfo.h"
+#include "arcatraz.h"
 
 enum ZerekethTexts
 {
@@ -30,9 +31,10 @@ enum ZerekethTexts
 enum ZerekethSpells
 {
     SPELL_VOID_ZONE                 = 36119,
-    SPELL_SHADOW_NOVA               = 36127,
     SPELL_SEED_OF_CORRUPTION        = 36123
 };
+
+#define SPELL_SHADOW_NOVA DUNGEON_MODE<uint32>(36127,39005)
 
 enum ZerekethEvents
 {
@@ -49,11 +51,21 @@ struct boss_zereketh_the_unbound : public BossAI
     void JustEngagedWith(Unit* who) override
     {
         BossAI::JustEngagedWith(who);
-        events.ScheduleEvent(EVENT_VOID_ZONE, 6s, 10s);
-        events.ScheduleEvent(EVENT_SHADOW_NOVA, 6s, 10s);
-        events.ScheduleEvent(EVENT_SEED_OF_CORRUPTION, 12s, 20s);
+        events.ScheduleEvent(EVENT_VOID_ZONE, 10s, 15s);
+        events.ScheduleEvent(EVENT_SHADOW_NOVA, 15s, 20s);
+        events.ScheduleEvent(EVENT_SEED_OF_CORRUPTION, 5s, 10s);
         Talk(SAY_AGGRO);
     }
+
+    void OnSpellCast(SpellInfo const* spell) override
+    {
+        if (spell->Id == SPELL_SHADOW_NOVA)
+            if (roll_chance(50))
+                Talk(SAY_SHADOW_NOVA);
+    }
+
+    // Do not despawn Void Zone
+    void JustSummoned(Creature* /*summon*/) override { }
 
     void KilledUnit(Unit* /*victim*/) override
     {
@@ -81,19 +93,18 @@ struct boss_zereketh_the_unbound : public BossAI
             switch (eventId)
             {
                 case EVENT_VOID_ZONE:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 1, 100, true))
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
                         DoCast(target, SPELL_VOID_ZONE);
-                    events.Repeat(6s, 10s);
+                    events.Repeat(15s);
                     break;
                 case EVENT_SHADOW_NOVA:
-                    DoCastVictim(SPELL_SHADOW_NOVA);
-                    Talk(SAY_SHADOW_NOVA);
-                    events.Repeat(6s, 10s);
+                    DoCastSelf(SPELL_SHADOW_NOVA);
+                    events.Repeat(15s, 20s);
                     break;
                 case EVENT_SEED_OF_CORRUPTION:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 1, 100, true))
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
                         DoCast(target, SPELL_SEED_OF_CORRUPTION);
-                    events.Repeat(12s, 20s);
+                    events.Repeat(15s, 20s);
                     break;
                 default:
                     break;
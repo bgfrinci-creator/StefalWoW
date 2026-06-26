TrinityCore/TrinityCore

Scripts/Stratholme: Rewrite scripts (#31232)

src/server/scripts/EasternKingdoms/Stratholme/boss_maleki_the_pallid.cpp
@@ -15,113 +15,104 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: boss_maleki_the_pallid
-SD%Complete: 100
-SDComment:
-SDCategory: Stratholme
-EndScriptData */
+/*
+ * Timers requires to be revisited
+ */
 
 #include "ScriptMgr.h"
 #include "InstanceScript.h"
 #include "ScriptedCreature.h"
 #include "stratholme.h"
 
-enum Spells
+enum MalekiSpells
 {
+    SPELL_FROST_ARMOR   = 12556,
     SPELL_FROSTBOLT     = 17503,
-    SPELL_DRAINLIFE     = 20743,
+    SPELL_DRAIN_LIFE    = 17238,
     SPELL_DRAIN_MANA    = 17243,
-    SPELL_ICETOMB       = 16869
+    SPELL_ICE_TOMB      = 16869
 };
 
 enum MalekiEvents
 {
     EVENT_FROSTBOLT     = 1,
-    EVENT_DRAINLIFE     = 2,
-    EVENT_DRAIN_MANA    = 3,
-    EVENT_ICETOMB       = 4
+    EVENT_DRAIN_LIFE,
+    EVENT_DRAIN_MANA,
+    EVENT_ICE_TOMB
 };
 
-class boss_maleki_the_pallid : public CreatureScript
+// 10438 - Maleki the Pallid
+struct boss_maleki_the_pallid : public ScriptedAI
 {
-public:
-    boss_maleki_the_pallid() : CreatureScript("boss_maleki_the_pallid") { }
+    boss_maleki_the_pallid(Creature* creature) : ScriptedAI(creature), _instance(creature->GetInstanceScript()) { }
 
-    struct boss_maleki_the_pallidAI : public ScriptedAI
+    void Reset() override
     {
-        boss_maleki_the_pallidAI(Creature* creature) : ScriptedAI(creature)
-        {
-            instance = me->GetInstanceScript();
-        }
-
-        void Reset() override
-        {
-            _events.Reset();
-        }
-
-        void JustEngagedWith(Unit* /*who*/) override
-        {
-            _events.ScheduleEvent(EVENT_FROSTBOLT, 1s);
-            _events.ScheduleEvent(EVENT_ICETOMB, 16s);
-            _events.ScheduleEvent(EVENT_DRAINLIFE, 31s);
-        }
-
-        void JustDied(Unit* /*killer*/) override
-        {
-            instance->SetData(TYPE_PALLID, IN_PROGRESS);
-        }
-
-        void UpdateAI(uint32 diff) override
-        {
-            //Return since we have no target
-            if (!UpdateVictim())
-                return;
-
-            _events.Update(diff);
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
-
-            while (uint32 eventId = _events.ExecuteEvent())
-            {
-                switch (eventId)
-                {
-                    case EVENT_FROSTBOLT:
-                        if (rand32() % 90)
-                            DoCastVictim(SPELL_FROSTBOLT);
-                        _events.ScheduleEvent(EVENT_FROSTBOLT, 3500ms);
-                        break;
-                    case EVENT_ICETOMB:
-                        if (rand32() % 65)
-                            DoCastVictim(SPELL_ICETOMB);
-                        _events.ScheduleEvent(EVENT_ICETOMB, 28s);
-                        break;
-                    case EVENT_DRAINLIFE:
-                        if (rand32() % 55)
-                            DoCastVictim(SPELL_DRAINLIFE);
-                        _events.ScheduleEvent(EVENT_DRAINLIFE, 31s);
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            DoMeleeAttackIfReady();
-        }
-
-    private:
-        EventMap _events;
-        InstanceScript* instance;
-    };
-
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return GetStratholmeAI<boss_maleki_the_pallidAI>(creature);
+        _events.Reset();
+        DoCastSelf(SPELL_FROST_ARMOR);
     }
+
+    void JustEngagedWith(Unit* /*who*/) override
+    {
+        _events.ScheduleEvent(EVENT_FROSTBOLT, 0s, 8s);
+        _events.ScheduleEvent(EVENT_DRAIN_LIFE, 20s, 25s);
+        _events.ScheduleEvent(EVENT_DRAIN_MANA, 15s, 25s);
+        _events.ScheduleEvent(EVENT_ICE_TOMB, 10s, 20s);
+    }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        _instance->SetData(TYPE_PALLID, IN_PROGRESS);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        _events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = _events.ExecuteEvent())
+        {
+            switch (eventId)
+            {
+                case EVENT_FROSTBOLT:
+                    DoCastVictim(SPELL_FROSTBOLT);
+                    _events.Repeat(2s, 8s);
+                    break;
+                case EVENT_DRAIN_LIFE:
+                    DoCastVictim(SPELL_DRAIN_LIFE);
+                    _events.Repeat(20s, 30s);
+                    break;
+                case EVENT_DRAIN_MANA:
+                {
+                    Unit* target = me->GetVictim();
+                    if (target && target->GetPowerType() == POWER_MANA)
+                        DoCast(target, SPELL_DRAIN_MANA);
+                    _events.Repeat(15s, 20s);
+                    break;
+                }
+                case EVENT_ICE_TOMB:
+                    DoCastVictim(SPELL_ICE_TOMB);
+                    _events.Repeat(15s, 25s);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        DoMeleeAttackIfReady();
+    }
+
+private:
+    EventMap _events;
+    InstanceScript* _instance;
 };
 
 void AddSC_boss_maleki_the_pallid()
 {
-    new boss_maleki_the_pallid();
+    RegisterStratholmeCreatureAI(boss_maleki_the_pallid);
 }

src/server/scripts/EasternKingdoms/Stratholme/boss_ramstein_the_gorger.cpp
@@ -15,106 +15,99 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: Boss_Ramstein_The_Gorger
-SD%Complete: 70
-SDComment:
-SDCategory: Stratholme
-EndScriptData */
+/*
+ * Timers requires to be revisited
+ * Intro NYI
+ */
 
 #include "ScriptMgr.h"
 #include "InstanceScript.h"
 #include "ScriptedCreature.h"
 #include "stratholme.h"
-#include "TemporarySummon.h"
 
-enum Spells
+enum RamsteinSpells
 {
+    SPELL_FLURRY            = 15088,
     SPELL_TRAMPLE           = 5568,
     SPELL_KNOCKOUT          = 17307
 };
 
-enum CreatureId
+enum RamsteinEvents
 {
-    NPC_MINDLESS_UNDEAD     = 11030
+    EVENT_TRAMPLE           = 1,
+    EVENT_KNOCKOUT
 };
 
-class boss_ramstein_the_gorger : public CreatureScript
+enum RamsteinMisc
 {
-public:
-    boss_ramstein_the_gorger() : CreatureScript("boss_ramstein_the_gorger") { }
+    SUMMON_GROUP_SENTRY     = 0,
+    SUMMON_GROUP_UNDEAD     = 1
+};
 
-    CreatureAI* GetAI(Creature* creature) const override
+// 10439 - Ramstein the Gorger
+struct boss_ramstein_the_gorger : public ScriptedAI
+{
+    boss_ramstein_the_gorger(Creature* creature) : ScriptedAI(creature), _instance(creature->GetInstanceScript()) { }
+
+    void Reset() override
     {
-        return GetStratholmeAI<boss_ramstein_the_gorgerAI>(creature);
+        _events.Reset();
+        DoCastSelf(SPELL_FLURRY);
     }
 
-    struct boss_ramstein_the_gorgerAI : public ScriptedAI
+    void JustEngagedWith(Unit* /*who*/) override
     {
-        boss_ramstein_the_gorgerAI(Creature* creature) : ScriptedAI(creature)
+        _events.ScheduleEvent(EVENT_TRAMPLE, 10s, 15s);
+        _events.ScheduleEvent(EVENT_KNOCKOUT, 10s, 20s);
+    }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        me->SummonCreatureGroup(SUMMON_GROUP_SENTRY);
+        me->SummonCreatureGroup(SUMMON_GROUP_UNDEAD);
+
+        _instance->SetData(TYPE_RAMSTEIN, DONE);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        _events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = _events.ExecuteEvent())
         {
-            Initialize();
-            instance = me->GetInstanceScript();
-        }
-
-        void Initialize()
-        {
-            Trample_Timer = 3000;
-            Knockout_Timer = 12000;
-        }
-
-        InstanceScript* instance;
-
-        uint32 Trample_Timer;
-        uint32 Knockout_Timer;
-
-        void Reset() override
-        {
-            Initialize();
-        }
-
-        void JustEngagedWith(Unit* /*who*/) override
-        {
-        }
-
-        void JustDied(Unit* /*killer*/) override
-        {
-            for (uint8 i = 0; i < 30; ++i)
+            switch (eventId)
             {
-                if (Creature* mob = me->SummonCreature(NPC_MINDLESS_UNDEAD, 3969.35f+irand(-10, 10), -3391.87f+irand(-10, 10), 119.11f, 5.91f, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 30min))
-                    mob->AI()->AttackStart(me->SelectNearestTarget(100.0f));
+                case EVENT_TRAMPLE:
+                    DoCastSelf(SPELL_TRAMPLE);
+                    _events.Repeat(10s, 25s);
+                    break;
+                case EVENT_KNOCKOUT:
+                    DoCastVictim(SPELL_KNOCKOUT);
+                    _events.Repeat(15s, 20s);
+                    break;
+                default:
+                    break;
             }
 
-            instance->SetData(TYPE_RAMSTEIN, DONE);
-        }
-
-        void UpdateAI(uint32 diff) override
-        {
-            //Return since we have no target
-            if (!UpdateVictim())
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
-
-            //Trample
-            if (Trample_Timer <= diff)
-            {
-                DoCast(me, SPELL_TRAMPLE);
-                Trample_Timer = 7000;
-            } else Trample_Timer -= diff;
-
-            //Knockout
-            if (Knockout_Timer <= diff)
-            {
-                DoCastVictim(SPELL_KNOCKOUT);
-                Knockout_Timer = 10000;
-            } else Knockout_Timer -= diff;
-
-            DoMeleeAttackIfReady();
         }
-    };
 
+        DoMeleeAttackIfReady();
+    }
+
+private:
+    EventMap _events;
+    InstanceScript* _instance;
 };
 
 void AddSC_boss_ramstein_the_gorger()
 {
-    new boss_ramstein_the_gorger();
+    RegisterStratholmeCreatureAI(boss_ramstein_the_gorger);
 }

src/server/scripts/EasternKingdoms/Stratholme/boss_timmy_the_cruel.cpp
@@ -15,89 +15,82 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: boss_timmy_the_cruel
-SD%Complete: 100
-SDComment:
-SDCategory: Stratholme
-EndScriptData */
+/*
+ * Timers requires to be revisited
+ */
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "stratholme.h"
 
-enum Says
+enum TimmyTexts
 {
-    SAY_SPAWN                   = 0
+    SAY_AGGRO               = 0
 };
 
-enum Spells
+enum TimmySpells
 {
-    SPELL_RAVENOUSCLAW          = 17470
+    SPELL_THRASH            = 12787,
+    SPELL_RAVENOUS_CLAW     = 17470
 };
 
-class boss_timmy_the_cruel : public CreatureScript
+enum TimmyEvents
 {
-public:
-    boss_timmy_the_cruel() : CreatureScript("boss_timmy_the_cruel") { }
+    EVENT_RAVENOUS_CLAW     = 1
+};
 
-    CreatureAI* GetAI(Creature* creature) const override
+// 10808 - Timmy the Cruel
+struct boss_timmy_the_cruel : public ScriptedAI
+{
+    boss_timmy_the_cruel(Creature* creature) : ScriptedAI(creature) { }
+
+    void Reset() override
     {
-        return GetStratholmeAI<boss_timmy_the_cruelAI>(creature);
+        _events.Reset();
+        DoCastSelf(SPELL_THRASH);
     }
 
-    struct boss_timmy_the_cruelAI : public ScriptedAI
+    void JustEngagedWith(Unit* /*who*/) override
     {
-        boss_timmy_the_cruelAI(Creature* creature) : ScriptedAI(creature)
-        {
-            Initialize();
-        }
+        Talk(SAY_AGGRO);
 
-        void Initialize()
-        {
-            RavenousClaw_Timer = 10000;
-            HasYelled = false;
-        }
+        _events.ScheduleEvent(EVENT_RAVENOUS_CLAW, 10s, 15s);
+    }
 
-        uint32 RavenousClaw_Timer;
-        bool HasYelled;
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
 
-        void Reset() override
-        {
-            Initialize();
-        }
+        _events.Update(diff);
 
-        void JustEngagedWith(Unit* /*who*/) override
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = _events.ExecuteEvent())
         {
-            if (!HasYelled)
+            switch (eventId)
             {
-                Talk(SAY_SPAWN);
-                HasYelled = true;
+                case EVENT_RAVENOUS_CLAW:
+                    DoCastVictim(SPELL_RAVENOUS_CLAW);
+                    _events.Repeat(10s, 15s);
+                    break;
+                default:
+                    break;
             }
-        }
 
-        void UpdateAI(uint32 diff) override
-        {
-            //Return since we have no target
-            if (!UpdateVictim())
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
-
-            //RavenousClaw
-            if (RavenousClaw_Timer <= diff)
-            {
-                //Cast
-                DoCastVictim(SPELL_RAVENOUSCLAW);
-                //15 seconds until we should cast this again
-                RavenousClaw_Timer = 15000;
-            } else RavenousClaw_Timer -= diff;
-
-            DoMeleeAttackIfReady();
         }
-    };
 
+        DoMeleeAttackIfReady();
+    }
+
+private:
+    EventMap _events;
 };
 
 void AddSC_boss_timmy_the_cruel()
 {
-    new boss_timmy_the_cruel();
+    RegisterStratholmeCreatureAI(boss_timmy_the_cruel);
 }

bgfrinci-creator/StefalWoW

(cherry picked from commit 2ce4c8640ea8ca3d3338091fdc053886e26272a8)

src/server/scripts/EasternKingdoms/Stratholme/boss_maleki_the_pallid.cpp
@@ -15,97 +15,96 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: boss_maleki_the_pallid
-SD%Complete: 100
-SDComment:
-SDCategory: Stratholme
-EndScriptData */
+/*
+ * Timers requires to be revisited
+ */
 
 #include "ScriptMgr.h"
 #include "InstanceScript.h"
 #include "ScriptedCreature.h"
 #include "stratholme.h"
 
-enum Spells
+enum MalekiSpells
 {
+    SPELL_FROST_ARMOR   = 12556,
     SPELL_FROSTBOLT     = 17503,
-    SPELL_DRAINLIFE     = 20743,
+    SPELL_DRAIN_LIFE    = 17238,
     SPELL_DRAIN_MANA    = 17243,
-    SPELL_ICETOMB       = 16869
+    SPELL_ICE_TOMB      = 16869
 };
 
 enum MalekiEvents
 {
     EVENT_FROSTBOLT     = 1,
-    EVENT_DRAINLIFE     = 2,
-    EVENT_DRAIN_MANA    = 3,
-    EVENT_ICETOMB       = 4
+    EVENT_DRAIN_LIFE,
+    EVENT_DRAIN_MANA,
+    EVENT_ICE_TOMB
 };
 
-class boss_maleki_the_pallid : public CreatureScript
+// 10438 - Maleki the Pallid
+struct boss_maleki_the_pallid : public BossAI
 {
-public:
-    boss_maleki_the_pallid() : CreatureScript("boss_maleki_the_pallid") { }
+    boss_maleki_the_pallid(Creature* creature) : BossAI(creature, BOSS_MALEKI_THE_PALLID) { }
 
-    struct boss_maleki_the_pallidAI : public BossAI
+    void Reset() override
     {
-        boss_maleki_the_pallidAI(Creature* creature) : BossAI(creature, BOSS_MALEKI_THE_PALLID)
+        BossAI::Reset();
+
+        DoCastSelf(SPELL_FROST_ARMOR);
+    }
+
+    void JustEngagedWith(Unit* who) override
+    {
+        BossAI::JustEngagedWith(who);
+
+        events.ScheduleEvent(EVENT_FROSTBOLT, 0s, 8s);
+        events.ScheduleEvent(EVENT_DRAIN_LIFE, 20s, 25s);
+        events.ScheduleEvent(EVENT_DRAIN_MANA, 15s, 25s);
+        events.ScheduleEvent(EVENT_ICE_TOMB, 10s, 20s);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-        }
-
-        void JustEngagedWith(Unit* who) override
-        {
-            _JustEngagedWith(who);
-            events.ScheduleEvent(EVENT_FROSTBOLT, 1s);
-            events.ScheduleEvent(EVENT_ICETOMB, 16s);
-            events.ScheduleEvent(EVENT_DRAINLIFE, 31s);
-        }
-
-        void UpdateAI(uint32 diff) override
-        {
-            //Return since we have no target
-            if (!UpdateVictim())
-                return;
-
-            events.Update(diff);
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
-
-            while (uint32 eventId = events.ExecuteEvent())
+            switch (eventId)
             {
-                switch (eventId)
+                case EVENT_FROSTBOLT:
+                    DoCastVictim(SPELL_FROSTBOLT);
+                    events.Repeat(2s, 8s);
+                    break;
+                case EVENT_DRAIN_LIFE:
+                    DoCastVictim(SPELL_DRAIN_LIFE);
+                    events.Repeat(20s, 30s);
+                    break;
+                case EVENT_DRAIN_MANA:
                 {
-                    case EVENT_FROSTBOLT:
-                        if (rand32() % 90)
-                            DoCastVictim(SPELL_FROSTBOLT);
-                        events.ScheduleEvent(EVENT_FROSTBOLT, 3500ms);
-                        break;
-                    case EVENT_ICETOMB:
-                        if (rand32() % 65)
-                            DoCastVictim(SPELL_ICETOMB);
-                        events.ScheduleEvent(EVENT_ICETOMB, 28s);
-                        break;
-                    case EVENT_DRAINLIFE:
-                        if (rand32() % 55)
-                            DoCastVictim(SPELL_DRAINLIFE);
-                        events.ScheduleEvent(EVENT_DRAINLIFE, 31s);
-                        break;
-                    default:
-                        break;
+                    Unit* target = me->GetVictim();
+                    if (target && target->GetPowerType() == POWER_MANA)
+                        DoCast(target, SPELL_DRAIN_MANA);
+                    events.Repeat(15s, 20s);
+                    break;
                 }
+                case EVENT_ICE_TOMB:
+                    DoCastVictim(SPELL_ICE_TOMB);
+                    events.Repeat(15s, 25s);
+                    break;
+                default:
+                    break;
             }
         }
-    };
-
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return GetStratholmeAI<boss_maleki_the_pallidAI>(creature);
     }
 };
 
 void AddSC_boss_maleki_the_pallid()
 {
-    new boss_maleki_the_pallid();
+    RegisterStratholmeCreatureAI(boss_maleki_the_pallid);
 }

src/server/scripts/EasternKingdoms/Stratholme/boss_ramstein_the_gorger.cpp
@@ -15,97 +15,95 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: Boss_Ramstein_The_Gorger
-SD%Complete: 70
-SDComment:
-SDCategory: Stratholme
-EndScriptData */
+/*
+ * Timers requires to be revisited
+ * Intro NYI
+ */
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "stratholme.h"
-#include "TemporarySummon.h"
 
-enum Spells
+enum RamsteinSpells
 {
+    SPELL_FLURRY            = 15088,
     SPELL_TRAMPLE           = 5568,
     SPELL_KNOCKOUT          = 17307
 };
 
-enum CreatureId
+enum RamsteinEvents
 {
-    NPC_MINDLESS_UNDEAD     = 11030
+    EVENT_TRAMPLE           = 1,
+    EVENT_KNOCKOUT
 };
 
-class boss_ramstein_the_gorger : public CreatureScript
+enum RamsteinMisc
 {
-public:
-    boss_ramstein_the_gorger() : CreatureScript("boss_ramstein_the_gorger") { }
+    SUMMON_GROUP_SENTRY     = 0,
+    SUMMON_GROUP_UNDEAD     = 1
+};
 
-    CreatureAI* GetAI(Creature* creature) const override
+// 10439 - Ramstein the Gorger
+struct boss_ramstein_the_gorger : public BossAI
+{
+    boss_ramstein_the_gorger(Creature* creature) : BossAI(creature, BOSS_RAMSTEIN_THE_GORGER) { }
+
+    void Reset() override
     {
-        return GetStratholmeAI<boss_ramstein_the_gorgerAI>(creature);
+        BossAI::Reset();
+
+        DoCastSelf(SPELL_FLURRY);
     }
 
-    struct boss_ramstein_the_gorgerAI : public BossAI
+    void JustEngagedWith(Unit* who) override
     {
-        boss_ramstein_the_gorgerAI(Creature* creature) : BossAI(creature, BOSS_RAMSTEIN_THE_GORGER)
+        BossAI::JustEngagedWith(who);
+
+        events.ScheduleEvent(EVENT_TRAMPLE, 10s, 15s);
+        events.ScheduleEvent(EVENT_KNOCKOUT, 10s, 20s);
+    }
+
+    void JustDied(Unit* killer) override
+    {
+        BossAI::JustDied(killer);
+
+        me->SummonCreatureGroup(SUMMON_GROUP_SENTRY);
+        me->SummonCreatureGroup(SUMMON_GROUP_UNDEAD);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            Initialize();
-        }
-
-        void Initialize()
-        {
-            Trample_Timer = 3000;
-            Knockout_Timer = 12000;
-        }
-
-        uint32 Trample_Timer;
-        uint32 Knockout_Timer;
-
-        void Reset() override
-        {
-            BossAI::Reset();
-
-            Initialize();
-        }
-
-        void JustDied(Unit* killer) override
-        {
-            BossAI::JustDied(killer);
-
-            for (uint8 i = 0; i < 30; ++i)
+            switch (eventId)
             {
-                if (Creature* mob = me->SummonCreature(NPC_MINDLESS_UNDEAD, 3969.35f+irand(-10, 10), -3391.87f+irand(-10, 10), 119.11f, 5.91f, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 30min))
-                    mob->AI()->AttackStart(me->SelectNearestTarget(100.0f));
+                case EVENT_TRAMPLE:
+                    DoCastSelf(SPELL_TRAMPLE);
+                    events.Repeat(10s, 25s);
+                    break;
+                case EVENT_KNOCKOUT:
+                    DoCastVictim(SPELL_KNOCKOUT);
+                    events.Repeat(15s, 20s);
+                    break;
+                default:
+                    break;
             }
-        }
 
-        void UpdateAI(uint32 diff) override
-        {
-            //Return since we have no target
-            if (!UpdateVictim())
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
-
-            //Trample
-            if (Trample_Timer <= diff)
-            {
-                DoCast(me, SPELL_TRAMPLE);
-                Trample_Timer = 7000;
-            } else Trample_Timer -= diff;
-
-            //Knockout
-            if (Knockout_Timer <= diff)
-            {
-                DoCastVictim(SPELL_KNOCKOUT);
-                Knockout_Timer = 10000;
-            } else Knockout_Timer -= diff;
         }
-    };
+    }
 };
 
 void AddSC_boss_ramstein_the_gorger()
 {
-    new boss_ramstein_the_gorger();
+    RegisterStratholmeCreatureAI(boss_ramstein_the_gorger);
 }

src/server/scripts/EasternKingdoms/Stratholme/boss_timmy_the_cruel.cpp
@@ -15,91 +15,80 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: boss_timmy_the_cruel
-SD%Complete: 100
-SDComment:
-SDCategory: Stratholme
-EndScriptData */
+/*
+ * Timers requires to be revisited
+ */
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "stratholme.h"
 
-enum Says
+enum TimmyTexts
 {
-    SAY_SPAWN                   = 0
+    SAY_AGGRO               = 0
 };
 
-enum Spells
+enum TimmySpells
 {
-    SPELL_RAVENOUSCLAW          = 17470
+    SPELL_THRASH            = 12787,
+    SPELL_RAVENOUS_CLAW     = 17470
 };
 
-class boss_timmy_the_cruel : public CreatureScript
+enum TimmyEvents
 {
-public:
-    boss_timmy_the_cruel() : CreatureScript("boss_timmy_the_cruel") { }
+    EVENT_RAVENOUS_CLAW     = 1
+};
 
-    CreatureAI* GetAI(Creature* creature) const override
+// 10808 - Timmy the Cruel
+struct boss_timmy_the_cruel : public BossAI
+{
+    boss_timmy_the_cruel(Creature* creature) : BossAI(creature, BOSS_TIMMY_THE_CRUEL) { }
+
+    void Reset() override
     {
-        return GetStratholmeAI<boss_timmy_the_cruelAI>(creature);
+        BossAI::Reset();
+
+        DoCastSelf(SPELL_THRASH);
     }
 
-    struct boss_timmy_the_cruelAI : public BossAI
+    void JustEngagedWith(Unit* who) override
     {
-        boss_timmy_the_cruelAI(Creature* creature) : BossAI(creature, BOSS_TIMMY_THE_CRUEL)
+        BossAI::JustEngagedWith(who);
+
+        Talk(SAY_AGGRO);
+
+        events.ScheduleEvent(EVENT_RAVENOUS_CLAW, 10s, 15s);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            Initialize();
-        }
-
-        void Initialize()
-        {
-            RavenousClaw_Timer = 10000;
-            HasYelled = false;
-        }
-
-        uint32 RavenousClaw_Timer;
-        bool HasYelled;
-
-        void Reset() override
-        {
-            BossAI::Reset();
-
-            Initialize();
-        }
-
-        void JustEngagedWith(Unit* who) override
-        {
-            BossAI::JustEngagedWith(who);
-
-            if (!HasYelled)
+            switch (eventId)
             {
-                Talk(SAY_SPAWN);
-                HasYelled = true;
+                case EVENT_RAVENOUS_CLAW:
+                    DoCastVictim(SPELL_RAVENOUS_CLAW);
+                    events.Repeat(10s, 15s);
+                    break;
+                default:
+                    break;
             }
-        }
 
-        void UpdateAI(uint32 diff) override
-        {
-            //Return since we have no target
-            if (!UpdateVictim())
+            if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
-
-            //RavenousClaw
-            if (RavenousClaw_Timer <= diff)
-            {
-                //Cast
-                DoCastVictim(SPELL_RAVENOUSCLAW);
-                //15 seconds until we should cast this again
-                RavenousClaw_Timer = 15000;
-            } else RavenousClaw_Timer -= diff;
         }
-    };
-
+    }
 };
 
 void AddSC_boss_timmy_the_cruel()
 {
-    new boss_timmy_the_cruel();
+    RegisterStratholmeCreatureAI(boss_timmy_the_cruel);
 }
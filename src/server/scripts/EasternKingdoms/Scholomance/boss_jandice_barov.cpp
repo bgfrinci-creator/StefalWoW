TrinityCore/TrinityCore

Scripts/Scholomance: Update scripts (#31333)

src/server/scripts/EasternKingdoms/Scholomance/boss_jandice_barov.cpp
@@ -15,55 +15,57 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Timers requires to be revisited
+ */
+
 #include "ScriptMgr.h"
 #include "scholomance.h"
 #include "ScriptedCreature.h"
 
+enum JandiceTexts
+{
+    EMOTE_DROP_JOURNAL          = 0
+};
+
 enum JandiceSpells
 {
-    SPELL_CURSE_OF_BLOOD        = 24673,
-    SPELL_ILLUSION              = 17773,
+    SPELL_CURSE_OF_BLOOD        = 16098,
+    SPELL_BANISH                = 8994,
+    SPELL_SUMMON_ILLUSION       = 17773,
+    SPELL_SPREAD                = 17774,
+
     SPELL_DROP_JOURNAL          = 26096
 };
 
 enum JandiceEvents
 {
-    EVENT_CURSE_OF_BLOOD = 1,
-    EVENT_ILLUSION,
-    EVENT_CLEAVE,
-    EVENT_SET_VISIBILITY
+    EVENT_CURSE_OF_BLOOD        = 1,
+    EVENT_BANISH,
+    EVENT_SUMMON_ILLUSION
 };
 
 // 10503 - Jandice Barov
 struct boss_jandice_barov : public ScriptedAI
 {
-    boss_jandice_barov(Creature* creature) : ScriptedAI(creature), _summons(me) { }
+    boss_jandice_barov(Creature* creature) : ScriptedAI(creature) { }
 
     void Reset() override
     {
         _events.Reset();
-        _summons.DespawnAll();
-    }
-
-    void JustSummoned(Creature* summoned) override
-    {
-        // Illusions should attack a random target.
-        if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
-            summoned->AI()->AttackStart(target);
-
-        _summons.Summon(summoned);
     }
 
     void JustEngagedWith(Unit* /*who*/) override
     {
-        _events.ScheduleEvent(EVENT_CURSE_OF_BLOOD, 15s);
-        _events.ScheduleEvent(EVENT_ILLUSION, 30s);
+        _events.ScheduleEvent(EVENT_CURSE_OF_BLOOD, 5s, 10s);
+        _events.ScheduleEvent(EVENT_BANISH, 10s, 15s);
+        _events.ScheduleEvent(EVENT_SUMMON_ILLUSION, 15s, 25s);
     }
 
     void JustDied(Unit* /*killer*/) override
     {
-        _summons.DespawnAll();
-        DoCastSelf(SPELL_DROP_JOURNAL, true);
+        DoCastSelf(SPELL_DROP_JOURNAL);
+        Talk(EMOTE_DROP_JOURNAL);
     }
 
     void UpdateAI(uint32 diff) override
@@ -81,20 +83,18 @@ struct boss_jandice_barov : public ScriptedAI
             switch (eventId)
             {
                 case EVENT_CURSE_OF_BLOOD:
-                    DoCastVictim(SPELL_CURSE_OF_BLOOD);
-                    _events.Repeat(30s);
+                    DoCastSelf(SPELL_CURSE_OF_BLOOD);
+                    _events.Repeat(25s, 30s);
                     break;
-                case EVENT_ILLUSION:
-                    DoCast(SPELL_ILLUSION);
-                    me->SetUnitFlag(UNIT_FLAG_UNINTERACTIBLE);
-                    me->SetDisplayId(11686);  // Invisible Model
-                    ModifyThreatByPercent(me->GetVictim(), -99);
-                    _events.ScheduleEvent(EVENT_SET_VISIBILITY, 3s);
-                    _events.Repeat(25s);
+                case EVENT_BANISH:
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                        DoCast(target, SPELL_BANISH);
+                    _events.Repeat(15s, 25s);
                     break;
-                case EVENT_SET_VISIBILITY:
-                    me->RemoveUnitFlag(UNIT_FLAG_UNINTERACTIBLE);
-                    me->SetDisplayId(11073);     // Jandice Model
+                case EVENT_SUMMON_ILLUSION:
+                    DoCastSelf(SPELL_SUMMON_ILLUSION);
+                    DoCastSelf(SPELL_SPREAD);
+                    _events.Repeat(30s, 40s);
                     break;
                 default:
                     break;
@@ -109,7 +109,6 @@ struct boss_jandice_barov : public ScriptedAI
 
 private:
     EventMap _events;
-    SummonList _summons;
 };
 
 void AddSC_boss_jandicebarov()

src/server/scripts/EasternKingdoms/Scholomance/boss_ras_frostwhisper.cpp
@@ -15,28 +15,32 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "scholomance.h"
+/*
+ * Timers requires to be revisited
+ */
+
 #include "ScriptMgr.h"
+#include "scholomance.h"
 #include "ScriptedCreature.h"
 
 enum RasSpells
 {
-    SPELL_FROSTBOLT         = 21369,
-    SPELL_ICE_ARMOR         = 18100, // This is actually a buff he gives himself
+    SPELL_ICE_ARMOR         = 18100,
     SPELL_FREEZE            = 18763,
-    SPELL_FEAR              = 26070,
+    SPELL_FEAR              = 12096,
     SPELL_CHILL_NOVA        = 18099,
+    SPELL_KNOCK_AWAY        = 11130,
     SPELL_FROSTBOLT_VOLLEY  = 8398
 };
 
 enum RasEvents
 {
-    EVENT_FROSTBOLT = 1,
-    EVENT_ICE_ARMOR,
+    EVENT_ICE_ARMOR         = 1,
     EVENT_FREEZE,
     EVENT_FEAR,
     EVENT_CHILL_NOVA,
-    EVENT_FROSTVOLLEY
+    EVENT_KNOCK_AWAY,
+    EVENT_FROSTBOLT_VOLLEY
 };
 
 // 10508 - Ras Frostwhisper
@@ -47,16 +51,18 @@ struct boss_boss_ras_frostwhisper : public ScriptedAI
     void Reset() override
     {
         _events.Reset();
+
         DoCastSelf(SPELL_ICE_ARMOR);
     }
 
     void JustEngagedWith(Unit* /*who*/) override
     {
-        _events.ScheduleEvent(EVENT_ICE_ARMOR, 2s);
-        _events.ScheduleEvent(EVENT_FROSTBOLT, 8s);
-        _events.ScheduleEvent(EVENT_CHILL_NOVA, 12s);
-        _events.ScheduleEvent(EVENT_FREEZE, 18s);
-        _events.ScheduleEvent(EVENT_FEAR, 45s);
+        _events.ScheduleEvent(EVENT_ICE_ARMOR, 30s);
+        _events.ScheduleEvent(EVENT_FREEZE, 20s, 30s);
+        _events.ScheduleEvent(EVENT_FEAR, 10s, 20s);
+        _events.ScheduleEvent(EVENT_CHILL_NOVA, 10s, 15s);
+        _events.ScheduleEvent(EVENT_KNOCK_AWAY, 20s, 30s);
+        _events.ScheduleEvent(EVENT_FROSTBOLT_VOLLEY, 10s, 20s);
     }
 
     void UpdateAI(uint32 diff) override
@@ -74,29 +80,30 @@ struct boss_boss_ras_frostwhisper : public ScriptedAI
             switch (eventId)
             {
                 case EVENT_ICE_ARMOR:
-                    DoCastSelf(SPELL_ICE_ARMOR);
-                    _events.Repeat(3min);
-                    break;
-                case EVENT_FROSTBOLT:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 40.0f, true))
-                        DoCast(target, SPELL_FROSTBOLT);
-                    _events.Repeat(8s);
+                    if (!me->HasAura(SPELL_ICE_ARMOR))
+                        DoCastSelf(SPELL_ICE_ARMOR);
+                    _events.Repeat(30s);
                     break;
                 case EVENT_FREEZE:
                     DoCastVictim(SPELL_FREEZE);
-                    _events.Repeat(24s);
+                    _events.Repeat(30s, 40s);
                     break;
                 case EVENT_FEAR:
-                    DoCastSelf(SPELL_FEAR);
-                    _events.Repeat(30s);
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                        DoCast(target, SPELL_FEAR);
+                    _events.Repeat(20s, 30s);
                     break;
                 case EVENT_CHILL_NOVA:
                     DoCastSelf(SPELL_CHILL_NOVA);
-                    _events.Repeat(14s);
+                    _events.Repeat(15s, 25s);
                     break;
-                case EVENT_FROSTVOLLEY:
-                    DoCastVictim(SPELL_FROSTBOLT_VOLLEY);
-                    _events.Repeat(15s);
+                case EVENT_KNOCK_AWAY:
+                    DoCastSelf(SPELL_KNOCK_AWAY);
+                    _events.Repeat(20s, 30s);
+                    break;
+                case EVENT_FROSTBOLT_VOLLEY:
+                    DoCastSelf(SPELL_FROSTBOLT_VOLLEY);
+                    _events.Repeat(10s, 20s);
                     break;
                 default:
                     break;

src/server/scripts/EasternKingdoms/Scholomance/boss_vectus.cpp
@@ -15,28 +15,28 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "scholomance.h"
-#include "ScriptMgr.h"
-#include "ScriptedCreature.h"
+/*
+ * Timers requires to be revisited
+ */
 
-enum VectusTexts
-{
-    EMOTE_FRENZY                 = 0
-};
+#include "ScriptMgr.h"
+#include "scholomance.h"
+#include "ScriptedCreature.h"
 
 enum VectusSpells
 {
-    SPELL_FLAMESTRIKE            = 18399,
+    // Passive
+    SPELL_FIRE_SHIELD            = 13377,
+
+    // Combat
     SPELL_BLAST_WAVE             = 16046,
-    SPELL_FIRE_SHIELD            = 19626,
-    SPELL_FRENZY                 = 8269  // 28371
+    SPELL_FLAMESTRIKE            = 18399
 };
 
 enum VectusEvents
 {
-    EVENT_FIRE_SHIELD = 1,
-    EVENT_BLAST_WAVE,
-    EVENT_FRENZY
+    EVENT_BLAST_WAVE             = 1,
+    EVENT_FLAMESTRIKE
 };
 
 // 10432 - Vectus
@@ -47,18 +47,14 @@ struct boss_vectus : public ScriptedAI
     void Reset() override
     {
         _events.Reset();
+
+        DoCastSelf(SPELL_FIRE_SHIELD);
     }
 
     void JustEngagedWith(Unit* /*who*/) override
     {
-        _events.ScheduleEvent(EVENT_FIRE_SHIELD, 2s);
-        _events.ScheduleEvent(EVENT_BLAST_WAVE, 14s);
-    }
-
-    void DamageTaken(Unit* /*attacker*/, uint32& damage, DamageEffectType /*damageType*/, SpellInfo const* /*spellInfo = nullptr*/) override
-    {
-        if (!me->HasAura(SPELL_FRENZY) && me->HealthBelowPctDamaged(25, damage))
-            _events.ScheduleEvent(EVENT_FRENZY, 0s);
+        _events.ScheduleEvent(EVENT_BLAST_WAVE, 15s, 25s);
+        _events.ScheduleEvent(EVENT_FLAMESTRIKE, 10s, 15s);
     }
 
     void UpdateAI(uint32 diff) override
@@ -75,18 +71,14 @@ struct boss_vectus : public ScriptedAI
         {
             switch (eventId)
             {
-                case EVENT_FIRE_SHIELD:
-                    DoCastSelf(SPELL_FIRE_SHIELD);
-                    _events.Repeat(90s);
-                    break;
                 case EVENT_BLAST_WAVE:
                     DoCastSelf(SPELL_BLAST_WAVE);
-                    _events.Repeat(12s);
+                    _events.Repeat(20s, 30s);
                     break;
-                case EVENT_FRENZY:
-                    DoCastSelf(SPELL_FRENZY);
-                    Talk(EMOTE_FRENZY);
-                    _events.Repeat(24s);
+                case EVENT_FLAMESTRIKE:
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                        DoCast(target, SPELL_FLAMESTRIKE);
+                    _events.Repeat(10s, 15s);
                     break;
                 default:
                     break;

bgfrinci-creator/StefalWoW

(cherry picked from commit 5a2cfd095c31b7785bf8a61e1b071873ffa25937)

src/server/scripts/EasternKingdoms/Scholomance/boss_jandice_barov.cpp
@@ -15,23 +15,34 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Timers requires to be revisited
+ */
+
 #include "ScriptMgr.h"
 #include "scholomance.h"
 #include "ScriptedCreature.h"
 
+enum JandiceTexts
+{
+    EMOTE_DROP_JOURNAL          = 0
+};
+
 enum JandiceSpells
 {
-    SPELL_CURSE_OF_BLOOD        = 24673,
-    SPELL_ILLUSION              = 17773,
+    SPELL_CURSE_OF_BLOOD        = 16098,
+    SPELL_BANISH                = 8994,
+    SPELL_SUMMON_ILLUSION       = 17773,
+    SPELL_SPREAD                = 17774,
+
     SPELL_DROP_JOURNAL          = 26096
 };
 
 enum JandiceEvents
 {
-    EVENT_CURSE_OF_BLOOD = 1,
-    EVENT_ILLUSION,
-    EVENT_CLEAVE,
-    EVENT_SET_VISIBILITY
+    EVENT_CURSE_OF_BLOOD        = 1,
+    EVENT_BANISH,
+    EVENT_SUMMON_ILLUSION
 };
 
 // 10503 - Jandice Barov
@@ -39,27 +50,19 @@ struct boss_jandice_barov : public BossAI
 {
     boss_jandice_barov(Creature* creature) : BossAI(creature, DATA_JANDICE_BAROV) { }
 
-    void JustSummoned(Creature* summoned) override
-    {
-        // Illusions should attack a random target.
-        if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
-            summoned->AI()->AttackStart(target);
-
-        summons.Summon(summoned);
-    }
-
     void JustEngagedWith(Unit* who) override
     {
         BossAI::JustEngagedWith(who);
-        events.ScheduleEvent(EVENT_CURSE_OF_BLOOD, 15s);
-        events.ScheduleEvent(EVENT_ILLUSION, 30s);
+        events.ScheduleEvent(EVENT_CURSE_OF_BLOOD, 5s, 10s);
+        events.ScheduleEvent(EVENT_BANISH, 10s, 15s);
+        events.ScheduleEvent(EVENT_SUMMON_ILLUSION, 15s, 25s);
     }
 
     void JustDied(Unit* killer) override
     {
         BossAI::JustDied(killer);
-        summons.DespawnAll();
-        DoCastSelf(SPELL_DROP_JOURNAL, true);
+        DoCastSelf(SPELL_DROP_JOURNAL);
+        Talk(EMOTE_DROP_JOURNAL);
     }
 
     void UpdateAI(uint32 diff) override
@@ -77,20 +80,18 @@ struct boss_jandice_barov : public BossAI
             switch (eventId)
             {
                 case EVENT_CURSE_OF_BLOOD:
-                    DoCastVictim(SPELL_CURSE_OF_BLOOD);
-                    events.Repeat(30s);
+                    DoCastSelf(SPELL_CURSE_OF_BLOOD);
+                    events.Repeat(25s, 30s);
                     break;
-                case EVENT_ILLUSION:
-                    DoCast(SPELL_ILLUSION);
-                    me->SetUninteractible(true);
-                    me->SetDisplayId(11686);  // Invisible Model
-                    ModifyThreatByPercent(me->GetVictim(), -99);
-                    events.ScheduleEvent(EVENT_SET_VISIBILITY, 3s);
-                    events.Repeat(25s);
+                case EVENT_BANISH:
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                        DoCast(target, SPELL_BANISH);
+                    events.Repeat(15s, 25s);
                     break;
-                case EVENT_SET_VISIBILITY:
-                    me->SetUninteractible(false);
-                    me->SetDisplayId(11073);     // Jandice Model
+                case EVENT_SUMMON_ILLUSION:
+                    DoCastSelf(SPELL_SUMMON_ILLUSION);
+                    DoCastSelf(SPELL_SPREAD);
+                    events.Repeat(30s, 40s);
                     break;
                 default:
                     break;

src/server/scripts/EasternKingdoms/Scholomance/boss_ras_frostwhisper.cpp
@@ -15,28 +15,32 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "scholomance.h"
+/*
+ * Timers requires to be revisited
+ */
+
 #include "ScriptMgr.h"
+#include "scholomance.h"
 #include "ScriptedCreature.h"
 
 enum RasSpells
 {
-    SPELL_FROSTBOLT         = 21369,
-    SPELL_ICE_ARMOR         = 18100, // This is actually a buff he gives himself
+    SPELL_ICE_ARMOR         = 18100,
     SPELL_FREEZE            = 18763,
-    SPELL_FEAR              = 26070,
+    SPELL_FEAR              = 12096,
     SPELL_CHILL_NOVA        = 18099,
+    SPELL_KNOCK_AWAY        = 11130,
     SPELL_FROSTBOLT_VOLLEY  = 8398
 };
 
 enum RasEvents
 {
-    EVENT_FROSTBOLT = 1,
-    EVENT_ICE_ARMOR,
+    EVENT_ICE_ARMOR         = 1,
     EVENT_FREEZE,
     EVENT_FEAR,
     EVENT_CHILL_NOVA,
-    EVENT_FROSTVOLLEY
+    EVENT_KNOCK_AWAY,
+    EVENT_FROSTBOLT_VOLLEY
 };
 
 // 10508 - Ras Frostwhisper
@@ -47,17 +51,19 @@ struct boss_boss_ras_frostwhisper : public BossAI
     void Reset() override
     {
         BossAI::Reset();
+
         DoCastSelf(SPELL_ICE_ARMOR);
     }
 
     void JustEngagedWith(Unit* who) override
     {
         BossAI::JustEngagedWith(who);
-        events.ScheduleEvent(EVENT_ICE_ARMOR, 2s);
-        events.ScheduleEvent(EVENT_FROSTBOLT, 8s);
-        events.ScheduleEvent(EVENT_CHILL_NOVA, 12s);
-        events.ScheduleEvent(EVENT_FREEZE, 18s);
-        events.ScheduleEvent(EVENT_FEAR, 45s);
+        events.ScheduleEvent(EVENT_ICE_ARMOR, 30s);
+        events.ScheduleEvent(EVENT_FREEZE, 20s, 30s);
+        events.ScheduleEvent(EVENT_FEAR, 10s, 20s);
+        events.ScheduleEvent(EVENT_CHILL_NOVA, 10s, 15s);
+        events.ScheduleEvent(EVENT_KNOCK_AWAY, 20s, 30s);
+        events.ScheduleEvent(EVENT_FROSTBOLT_VOLLEY, 10s, 20s);
     }
 
     void UpdateAI(uint32 diff) override
@@ -75,29 +81,30 @@ struct boss_boss_ras_frostwhisper : public BossAI
             switch (eventId)
             {
                 case EVENT_ICE_ARMOR:
-                    DoCastSelf(SPELL_ICE_ARMOR);
-                    events.Repeat(3min);
-                    break;
-                case EVENT_FROSTBOLT:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 40.0f, true))
-                        DoCast(target, SPELL_FROSTBOLT);
-                    events.Repeat(8s);
+                    if (!me->HasAura(SPELL_ICE_ARMOR))
+                        DoCastSelf(SPELL_ICE_ARMOR);
+                    events.Repeat(30s);
                     break;
                 case EVENT_FREEZE:
                     DoCastVictim(SPELL_FREEZE);
-                    events.Repeat(24s);
+                    events.Repeat(30s, 40s);
                     break;
                 case EVENT_FEAR:
-                    DoCastSelf(SPELL_FEAR);
-                    events.Repeat(30s);
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                        DoCast(target, SPELL_FEAR);
+                    events.Repeat(20s, 30s);
                     break;
                 case EVENT_CHILL_NOVA:
                     DoCastSelf(SPELL_CHILL_NOVA);
-                    events.Repeat(14s);
+                    events.Repeat(15s, 25s);
                     break;
-                case EVENT_FROSTVOLLEY:
-                    DoCastVictim(SPELL_FROSTBOLT_VOLLEY);
-                    events.Repeat(15s);
+                case EVENT_KNOCK_AWAY:
+                    DoCastSelf(SPELL_KNOCK_AWAY);
+                    events.Repeat(20s, 30s);
+                    break;
+                case EVENT_FROSTBOLT_VOLLEY:
+                    DoCastSelf(SPELL_FROSTBOLT_VOLLEY);
+                    events.Repeat(10s, 20s);
                     break;
                 default:
                     break;

src/server/scripts/EasternKingdoms/Scholomance/boss_vectus.cpp
@@ -15,13 +15,16 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "scholomance.h"
+/*
+ * Timers requires to be revisited
+ */
+
 #include "ScriptMgr.h"
+#include "scholomance.h"
 #include "ScriptedCreature.h"
 
 enum VectusTexts
 {
-    EMOTE_FRENZY                 = 0,
     TALK_IDLE                    = 1,
     TALK_IDLE2                   = 2,
     TALK_IDLE3                   = 3
@@ -29,17 +32,18 @@ enum VectusTexts
 
 enum VectusSpells
 {
-    SPELL_FLAMESTRIKE            = 18399,
+    // Passive
+    SPELL_FIRE_SHIELD            = 13377,
+
+    // Combat
     SPELL_BLAST_WAVE             = 16046,
-    SPELL_FIRE_SHIELD            = 19626,
-    SPELL_FRENZY                 = 8269  // 28371
+    SPELL_FLAMESTRIKE            = 18399
 };
 
 enum VectusEvents
 {
-    EVENT_FIRE_SHIELD = 1,
-    EVENT_BLAST_WAVE,
-    EVENT_FRENZY
+    EVENT_BLAST_WAVE             = 1,
+    EVENT_FLAMESTRIKE
 };
 
 enum VectusPaths
@@ -55,18 +59,14 @@ struct boss_vectus : public BossAI
     void Reset() override
     {
         events.Reset();
+
+        DoCastSelf(SPELL_FIRE_SHIELD);
     }
 
     void JustEngagedWith(Unit* /*who*/) override
     {
-        events.ScheduleEvent(EVENT_FIRE_SHIELD, 2s);
-        events.ScheduleEvent(EVENT_BLAST_WAVE, 14s);
-    }
-
-    void DamageTaken(Unit* /*attacker*/, uint32& damage, DamageEffectType /*damageType*/, SpellInfo const* /*spellInfo = nullptr*/) override
-    {
-        if (!me->HasAura(SPELL_FRENZY) && me->HealthBelowPctDamaged(25, damage))
-            events.ScheduleEvent(EVENT_FRENZY, 0s);
+        events.ScheduleEvent(EVENT_BLAST_WAVE, 15s, 25s);
+        events.ScheduleEvent(EVENT_FLAMESTRIKE, 10s, 15s);
     }
 
     void UpdateAI(uint32 diff) override
@@ -83,18 +83,14 @@ struct boss_vectus : public BossAI
         {
             switch (eventId)
             {
-                case EVENT_FIRE_SHIELD:
-                    DoCastSelf(SPELL_FIRE_SHIELD);
-                    events.Repeat(90s);
-                    break;
                 case EVENT_BLAST_WAVE:
                     DoCastSelf(SPELL_BLAST_WAVE);
-                    events.Repeat(12s);
+                    events.Repeat(20s, 30s);
                     break;
-                case EVENT_FRENZY:
-                    DoCastSelf(SPELL_FRENZY);
-                    Talk(EMOTE_FRENZY);
-                    events.Repeat(24s);
+                case EVENT_FLAMESTRIKE:
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                        DoCast(target, SPELL_FLAMESTRIKE);
+                    events.Repeat(10s, 15s);
                     break;
                 default:
                     break;
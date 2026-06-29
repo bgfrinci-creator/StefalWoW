TrinityCore/TrinityCore

Core/Movement: Check that movement generators were properly initialized and fix off-by-one-update-tick time tracking (#31612)

src/server/game/Movement/MovementGenerators/WaypointMovementGenerator.cpp
@@ -97,7 +97,7 @@ bool WaypointMovementGenerator<Creature>::GetResetPosition(Unit* /*owner*/, floa
     return true;
 }
 
-void WaypointMovementGenerator<Creature>::DoInitialize(Creature* owner)
+bool WaypointMovementGenerator<Creature>::DoInitialize(Creature* owner)
 {
     RemoveFlag(MOVEMENTGENERATOR_FLAG_INITIALIZATION_PENDING | MOVEMENTGENERATOR_FLAG_TRANSITORY | MOVEMENTGENERATOR_FLAG_DEACTIVATED);
 
@@ -112,15 +112,16 @@ void WaypointMovementGenerator<Creature>::DoInitialize(Creature* owner)
     if (!_path)
     {
         TC_LOG_ERROR("sql.sql", "WaypointMovementGenerator::DoInitialize: couldn't load path for creature ({}) (_pathId: {})", owner->GetGUID().ToString(), _pathId);
-        return;
+        return false;
     }
 
     owner->StopMoving();
 
     _nextMoveTime.Reset(1000);
+    return true;
 }
 
-void WaypointMovementGenerator<Creature>::DoReset(Creature* owner)
+bool WaypointMovementGenerator<Creature>::DoReset(Creature* owner)
 {
     RemoveFlag(MOVEMENTGENERATOR_FLAG_TRANSITORY | MOVEMENTGENERATOR_FLAG_DEACTIVATED);
 
@@ -128,6 +129,7 @@ void WaypointMovementGenerator<Creature>::DoReset(Creature* owner)
 
     if (!HasFlag(MOVEMENTGENERATOR_FLAG_FINALIZED) && _nextMoveTime.Passed())
         _nextMoveTime.Reset(1); // Needed so that Update does not behave as if node was reached
+    return true;
 }
 
 bool WaypointMovementGenerator<Creature>::DoUpdate(Creature* owner, uint32 diff)

bgfrinci-creator/StefalWoW

(cherry picked from commit 8787dfe6cfca3c5eb32fb024bdaa5e83d725aab8)

src/server/game/Movement/MovementGenerators/WaypointMovementGenerator.cpp
@@ -137,7 +137,7 @@ bool WaypointMovementGenerator<T>::GetResetPosition(Unit* /*owner*/, float& x, f
 }
 
 template <typename T>
-void WaypointMovementGenerator<T>::DoInitialize(T* owner)
+bool WaypointMovementGenerator<T>::DoInitialize(T* owner)
 {
     this->RemoveFlag(MOVEMENTGENERATOR_FLAG_INITIALIZATION_PENDING | MOVEMENTGENERATOR_FLAG_TRANSITORY | MOVEMENTGENERATOR_FLAG_DEACTIVATED);
 
@@ -145,7 +145,7 @@ void WaypointMovementGenerator<T>::DoInitialize(T* owner)
     if (!path)
     {
         TC_LOG_ERROR("sql.sql", "WaypointMovementGenerator::DoInitialize: couldn't load path for {}", owner->GetGUID());
-        return;
+        return false;
     }
 
     if (path->Nodes.size() == 1)
@@ -154,10 +154,11 @@ void WaypointMovementGenerator<T>::DoInitialize(T* owner)
     owner->StopMoving();
 
     _nextMoveTime.Reset(1000);
+    return true;
 }
 
 template <typename T>
-void WaypointMovementGenerator<T>::DoReset(T* owner)
+bool WaypointMovementGenerator<T>::DoReset(T* owner)
 {
     this->RemoveFlag(MOVEMENTGENERATOR_FLAG_TRANSITORY | MOVEMENTGENERATOR_FLAG_DEACTIVATED);
 
@@ -165,6 +166,7 @@ void WaypointMovementGenerator<T>::DoReset(T* owner)
 
     if (!this->HasFlag(MOVEMENTGENERATOR_FLAG_FINALIZED) && _nextMoveTime.Passed())
         _nextMoveTime.Reset(1); // Needed so that Update does not behave as if node was reached
+    return true;
 }
 
 template <typename T>
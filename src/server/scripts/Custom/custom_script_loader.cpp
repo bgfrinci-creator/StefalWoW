87640ea4c584fa03eea800f15d8f464270b263b7
stevebone <zavoianu.stefan@gmail.com>
2026-09-05 18:21:48 +0200

    Scripts/Paladin: implement the new 406064 Art of War talent
    Original patch from Nokreb.

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -30,6 +30,7 @@ void AddSC_custom_hunter_spell_fixes();
 void AddSC_custom_mage_spell_fixes();
 void AddSC_custom_monk_spell_npcs();
 void AddSC_custom_monk_spell_fixes();
+void AddSC_custom_paladin_spell_fixes();
 void AddSC_custom_shaman_totem_npcs();
 void AddSC_custom_warlock_spell_fixes();
 void AddSC_custom_warlock_demon_npcs();
@@ -135,6 +136,7 @@ void AddCustomScripts()
     AddSC_custom_mage_spell_fixes();
     AddSC_custom_monk_spell_npcs();
     AddSC_custom_monk_spell_fixes();
+    AddSC_custom_paladin_spell_fixes();
     AddSC_custom_shaman_totem_npcs();
     AddSC_custom_warlock_spell_fixes();
     AddSC_custom_warlock_demon_npcs();

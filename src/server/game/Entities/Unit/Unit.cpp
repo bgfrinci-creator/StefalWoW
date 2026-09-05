369417b1abfd62e3fe51c91ae384c027295b4333
Ovahlord <18347559+ovahlord@users.noreply.github.com>
2026-09-05 17:57:54 +0200

    Core/Units: rename new parameter in Unit::DisableGravity to reflect better what it does

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13519,7 +13519,7 @@ bool Unit::SetWalk(bool enable)
     return true;
 }
 
-bool Unit::SetDisableGravity(bool disable, bool updateAnimTier /*= true*/, bool playHoverAnim /*= true*/)
+bool Unit::SetDisableGravity(bool disable, bool updateAnimTier /*= true*/, bool updatePlayHoverAnim /*= true*/)
 {
     if (disable == IsGravityDisabled())
         return false;

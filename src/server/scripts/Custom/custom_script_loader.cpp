2903651700b3493949ba2d2157aa88ad7f38838b
stevebone <zavoianu.stefan@gmail.com>
2026-09-17 20:46:37 +0200

    Scripts/Spells: moved archaeology to the custom folder since it is not native TC

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -115,6 +115,7 @@ void AddSC_npc_perks_program_vendor();
 
 // ==================== Spells Scripts ======================= //
 void AddSC_advanced_flying_spell_scripts();
+void AddSC_archaeology_spell_scripts();
 
 // ==================== Misc Scripts ========================= //
 void AddSC_gm_fixer();
@@ -220,6 +221,7 @@ void AddCustomScripts()
 
     // Spells
     AddSC_advanced_flying_spell_scripts();
+    AddSC_archaeology_spell_scripts();
 
     // Misc
     AddSC_gm_fixer();
